f625f02e9f9f2c6622fc877482bb69b4ab952169
Kanishke <kanishkegb@gwu.edu>
2017-07-20 13:54:29 -0400

    plots without params

--- ezplot.py
+++ ezplot.py
@@ -1,5 +1,7 @@
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import numpy as np
+import pdb
 
 class SubPlot:
     ''' subplot class '''
@@ -19,34 +21,52 @@ class SubPlot:
         self.cols = cols
         self.max_ax = rows * cols
 
+        self.line_mode = 0
+        if rows==1 or cols==1:
+            self.line_mode = 1
+
         self.fig, self.ax = plt.subplots(rows, cols)
 
         return
 
 
-    def subplot(self, subplot_number):
+    def subplot(self, subplot_number_r, subplot_number_c=0):
 
-        if not isinstance(subplot_number, int):
+        if not isinstance(subplot_number_r, int) or not isinstance(subplot_number_c, int):
             print('ezplot error: subplot index must be an integer.')
             return
 
-        if subplot_number > self.max_ax:
-            print('ezplot error: subplot index must be less than rows x columns')
+        if subplot_number_r >= self.rows:
+            print('ezplot error: subplot row index must be less than max row')
             return
 
-        if subplot_number < 0:
-            print('ezplot error: subplot index must be a positive integer.')
+        if subplot_number_c >= self.cols:
+            print('ezplot error: subplot column index must be less than max columns')
             return
 
-        self.n_ax = subplot_number
+        if subplot_number_c < 0 or subplot_number_r < 0:
+            print('ezplot error: subplot indices must be a positive integers.')
+            return
+
+        if self.line_mode:
+            self.ax_no = subplot_number_r
+        else:
+            self.ax_no = (subplot_number_r, subplot_number_c)
 
         return
 
 
     def suptitle(self, title):
         self.fig.suptitle(title, fontsize=12)
-
         return
 
-    def plot(self, params):
+
+    def ez_plot(self, x, y, params=None):
+        # params = ('r', linewidth=2)
+
+        if self.line_mode:
+            line = self.ax[self.ax_no].plot(x[:], y[:])
+        else:
+            line = self.ax[self.ax_no].plot(x[:], y[:])
+
         return
--- test.py
+++ test.py
@@ -1,13 +1,16 @@
-import matplotlib.pyplot as plt
 import ezplot as ezp
+import matplotlib.pyplot as plt
+import numpy as np
 
 if __name__ == "__main__":
 
-    fig1 = ezp.SubPlot(3, 2)
-    print(fig1.rows)
-    print(fig1.cols)
+    x = np.arange(1, 10)
+    y = x
 
-    fig1.subplot(1)
+    fig1 = ezp.SubPlot(2, 3)
     fig1.suptitle('Test1')
+
+    fig1.subplot(0, 1)
+    fig1.ez_plot(x, y)
 
     plt.show()
