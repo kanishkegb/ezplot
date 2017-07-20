9c7a8db39a2b62a471fa2846005bd6847d98f285
Kanishke <kanishkegb@gwu.edu>
2017-07-20 11:26:08 -0400

    started SubPlot class

--- ezplot.py
+++ ezplot.py
@@ -1,2 +1,9 @@
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+
+class SubPlot:
+    ''' sub plot class '''
+
+    def __init__(self, rows, cols):
+        self.rows = rows
+        self.cols = cols
---
+++ test.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import ezplot as ezp
+
+if __name__ == "__main__":
+
+    fig1 = ezp.SubPlot(3, 2)
+    print(fig1.rows)
+    print(fig1.cols)
