f4c7524abbdbf8aef30c53dbcfd3f9e5f9067c0d
Kanishke <kanishkegb@gwu.edu>
2017-07-20 12:13:14 -0400

    opens a subplot figure

--- ezplot.py
+++ ezplot.py
@@ -2,8 +2,51 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 class SubPlot:
-    ''' sub plot class '''
+    ''' subplot class '''
+
 
     def __init__(self, rows, cols):
+
+        if not isinstance(rows, int):
+            print('ezplot error: Number of subplot rows must be an integer.')
+            return
+
+        if not isinstance(cols, int):
+            print('ezplot error: Number of subplot columns must be an integer.')
+            return
+
         self.rows = rows
         self.cols = cols
+        self.max_ax = rows * cols
+
+        self.fig, self.ax = plt.subplots(rows, cols)
+
+        return
+
+
+    def subplot(self, subplot_number):
+
+        if not isinstance(subplot_number, int):
+            print('ezplot error: subplot index must be an integer.')
+            return
+
+        if subplot_number > self.max_ax:
+            print('ezplot error: subplot index must be less than rows x columns')
+            return
+
+        if subplot_number < 0:
+            print('ezplot error: subplot index must be a positive integer.')
+            return
+
+        self.n_ax = subplot_number
+
+        return
+
+
+    def suptitle(self, title):
+        self.fig.suptitle(title, fontsize=12)
+
+        return
+
+    def plot(self, params):
+        return
--- test.py
+++ test.py
@@ -6,3 +6,8 @@ if __name__ == "__main__":
     fig1 = ezp.SubPlot(3, 2)
     print(fig1.rows)
     print(fig1.cols)
+
+    fig1.subplot(1)
+    fig1.suptitle('Test1')
+
+    plt.show()
