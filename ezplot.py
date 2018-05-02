23b46b1eb6273f87e83f4d7bb99a3fc5ca221c9d
Kani-Desktop <kanishkegb@gwu.edu>
2018-05-02 02:09:51 -0400

    plot working except non-single column subplots

--- ezplot.py
+++ ezplot.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 
+import copy
 import itertools
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -10,97 +11,145 @@ import pdb
 def plot(*args, **kwargs):
 
     x, y, num_subplots = select_mode(*args)
-    # y.append(np.arange(0, 10))
-
-    # if mode=='single':
-    #     plt.figure()
-    #     plt.plot(x[0], **kwargs)
-    # elif mode=='xy':
-    #     if not num_subplots:
-    #         plt.figure()
-    #         for i in range(len(x)):
-    #             plt.plot(x[i], y[i], **kwargs)
-    #     else:
     m, n = num_subplots
-    print('m: {}, n: {}'.format(m, n))
     fig, axes = plt.subplots(m, n)
     for i, j in list(itertools.product(range(len(y)), range(m*n))):
-        print('{}, {}'.format(i, j))
+        print(j)
+        pdb.set_trace()
         try:
             ax = axes[j]
         except TypeError:
             ax = axes
-            if len(y)==1 and m==1:
-                print(1)
-                y = [y]
-        # pdb.set_trace()
-        ax.plot(x[i], y[i][j], **kwargs)
+        ax.plot(x[i][j, :], y[i][j, :], **kwargs)
 
     return
 
 
 def select_mode(*args):
 
+    num_args = len(args)
     all_x = []
     all_y = []
     len_x = 0
+
+    if num_args % 2 and not num_args == 1:
+        raise ValueError('data must be in the format (x1, y1, x2, y2, ...)')
+
+    mid  = int(num_args/2)
+    for i in range(mid):
+        x_m, x_n = get_size(args[i])
+        y_m, y_n = get_size(args[mid + i])
+        if not x_n == y_n:
+            raise ValueError('each (x, y) must have same lengths\n'
+                             'for pair {}, the length of x is {}, but the '
+                             'length of y is {}'.format(i % 2, len(args[i]),
+                             len(args[mid + i])))
     flag_first_run = True
     for i, arg in enumerate(args):
         if i % 2:
-            m, n, transpose = get_size(arg)
-            if flag_first_run:
-                flag_first_run = False
-                num_data_series = m
-            num_data_points = n
-            if not num_data_points==n:
-                raise ValueError('each (x, y) must have same lengths\n'
-                'for pair {}, the length of x is {}, but the length of y is '
-                '{}'.format(i % 2, len_x, len_y))
-
-            if transpose:
+            m, n = get_size(arg)
+            if m > n:
+                m, n = n, m
                 all_y.append(arg.T)
             else:
                 all_y.append(arg)
+
+            if flag_first_run:
+                flag_first_run = False
+                num_data_series = m
+
         else:
-            m, n, transpose = get_size(arg)
+            m, n = get_size(arg)
             if flag_first_run:
                 num_data_series = m
             num_data_points = n
 
-            if transpose:
+            if m > n:
+                m, n = n, m
                 all_x.append(arg.T)
             else:
                 all_x.append(arg)
 
-    if i==0:
-        return [np.arange(0, num_data_points)], all_x, (m, 1)
+    x, y = clean_xy(all_x, all_y)
+    print(m)
+    n_rows, n_cols = get_subplot_dims(m)
+    print(str(n_rows) + ' ' + str(n_cols))
+
+    return x, y, (n_rows, n_cols)
 
 
-    elif not i % 2:
-        raise ValueError('data must be in the format (x1, y1, x2, y2, ...)')
+def clean_xy(all_x, all_y):
+
+    num_series = len(all_x)
+    x = [[] for i in range(num_series)]
+    for i, data_series in enumerate(all_x):
+        num_axes, num_data_points = get_size(data_series)
+        x[i] = np.zeros((num_axes, num_data_points))
+        for ax in range(num_axes):
+            if num_axes == 1:
+                x[i][ax, :] = data_series
+            else:
+                x[i][ax, :] = data_series[ax, :]
+
+    if len(all_y) == 0:
+        y = copy.deepcopy(x)
+        num_axes, num_data_points = get_size(data_series)
+        x[i] = np.zeros((num_axes, num_data_points))
+        for i, data_series in enumerate(y):
+            num_axes, num_data_points = get_size(data_series)
+            x[i] = np.zeros((num_axes, num_data_points))
+            for ax in range(num_axes):
+                x[i][ax, :] = np.arange(0, len(data_series[0]))
     else:
-        mode = 'xy'
-        m, n = np.shape(all_y[0])
-        num_subplots = min([m, n])
+        y = [[] for num_series in range(num_series)]
+        for i, data_series in enumerate(all_y):
+            num_axes, num_data_points = get_size(data_series)
+            y[i] = np.zeros((num_axes, num_data_points))
+            for ax in range(num_axes):
+                if num_axes == 1:
+                    y[i][ax, :] = data_series
+                else:
+                    y[i][ax, :] = data_series[ax, :]
 
-    return all_x, all_y, (num_subplots, 1)
+        # this part is needed for matching x values with multiple data series
+        x = [[] for i in range(num_series)]
+        for i, data_series in enumerate(all_x):
+            temp, num_data_points = get_size(data_series)
+            x[i] = np.zeros((num_axes, num_data_points))
+            for ax in range(num_axes):
+                x[i][ax, :] = data_series
+
+    return x, y
+
+
+
+
+def get_subplot_dims(m):
+
+    if m==4:
+        n_rows = 2
+        n_cols = 2
+    elif m==6:
+        n_rows = 3
+        n_cols = 2
+    elif m==9:
+        n_rows = 3
+        n_cols = 3
+    else:
+        n_rows = 1
+        n_cols = m
+
+    return n_rows, n_cols
 
 
 def get_size(arr):
     try:
         m, n = np.shape(arr)
-        if m > n:
-            m, n = n, m
-            transpose_flag = True
-        else:
-            transpose_flag = False
-
     except ValueError:
         m = 1
         n = len(arr)
-        transpose_flag = False
 
-    return m, n, transpose_flag
+    return m, n
 
 
 if __name__ == "__main__":
@@ -108,9 +157,10 @@ if __name__ == "__main__":
     x = np.arange(0, 10)
 
     # plot(x, 2*x, 3*x, x, linestyle='dashed')
-    plot(x, color='green')
+    # plot(x, color='black')
     # plot(x, np.array([x, 2*x]), color='green')
-    # plot(np.array([x, 2*x]), color='green')
-    plot(np.random.rand(2, 10), color='green')
+    # plot(np.array([x, 2*x]), color='red')
+    # plot(np.random.rand(2, 10), color='blue')
+    plot(np.random.rand(9, 10))
     # pdb.set_trace()
     plt.show()
